372d90c646da5af24e144f40798a93f3ff526035
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-17 20:28:17 +0200

    Failing tests.

---
+++ integrationtests/Paket.IntegrationTests/ConvertFromNuGetNuspecSpecs.fs
@@ -0,0 +1,37 @@
+﻿module Paket.IntegrationTests.ConvertFromNuGetNuspecSpecs
+
+open Fake
+open System
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Diagnostics
+open Paket
+open Paket.Domain
+open Paket.Requirements
+
+[<Test>]
+let ``#??? should convert C# project with nuspec``() = 
+    Assert.Fail("placeholder to get a PR-number before adding scenario files")
+    Assert.Fail("check the created .template file for:")
+    Assert.Fail("test nuspec containing $author$")
+    Assert.Fail("test frameworkdependencies")
+    Assert.Fail("test project url")
+    Assert.Fail("test license")
+    Assert.Fail("test that nuget folder is cleaned up")
+    Assert.Fail("test that NugetPowerTools dependency is removed")
+
+[<Test>]
+let ``#??? should convert C# project with nuspec and package dependencies``() = 
+    Assert.Fail("placeholder to get a PR-number before adding scenario files")
+
+[<Test>]
+let ``#??? should convert C# projects with package interdependencies``() = 
+    Assert.Fail("placeholder to get a PR-number before adding scenario files")
+    Assert.Fail("test that package references other package created in sln")
+
+[<Test>]
+let ``#??? should convert C# projects where package contains binaries from both projects``() = 
+    Assert.Fail("placeholder to get a PR-number before adding scenario files")
+
